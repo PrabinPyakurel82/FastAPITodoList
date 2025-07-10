702b5a0ad9e8811db3228ae7d2c1c5d8b8c6704e
PrabinPyakurel82 <prabinpyakurel82@gmail.com>
2025-07-10 23:38:04 +0545

    updated database model Todo

--- app/core/deps.py
+++ app/core/deps.py
@@ -4,12 +4,15 @@ from jose import jwt, JWTError
 from sqlalchemy.orm import Session
 
 
-from db import get_db
+from core.db import get_db
 from models import User
-from security import ALGORITHM, SECRET_KEY
+from core.security import ALGORITHM, SECRET_KEY
 
 
-def get_current_user(token:str = Depends(OAuth2PasswordBearer), db: Session = Depends(get_db)):
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+
+def get_current_user(token:str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     payload = jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
     user_id: str = payload.get('sub')
 
@@ -20,7 +23,7 @@ def get_current_user(token:str = Depends(OAuth2PasswordBearer), db: Session = De
             headers={"WWW-Authenticate": "Bearer"},
         )
      
-    user = db.query(User).get(User.id == user.id)
+    user = db.query(User).filter(User.id == int(user_id)).first()
     if user is None:
         raise HTTPException(
             status_code = status.HTTP_401_UNAUTHORIZED,
--- app/models.py
+++ app/models.py
@@ -14,6 +14,7 @@ class User(Base):
 class Todo(Base):
     __tablename__ = 'todos'
     id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     task = Column(String, nullable=False)
     completed = Column(Boolean, default=False)
     created_at = Column(DateTime, server_default=func.now())
